708fa14be6d0e701d8e51e4891a17187f59e3bd4
gamitarchana <45191739+gamitarchana@users.noreply.github.com>
2019-07-01 14:45:18 +0530

    Added settings for mysql, logging & azure storage

--- outstation/settings/base.py
+++ outstation/settings/base.py
@@ -246,51 +246,51 @@ LOGGING = {
 	'version': 1,
 	'disable_existing_loggers': False,
 	'filters': {
-	'require_debug_false': {
-		'()': 'django.utils.log.RequireDebugFalse',
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse',
 		},
-	'require_debug_true': {
-		'()': 'django.utils.log.RequireDebugTrue',
+		'require_debug_true': {
+			'()': 'django.utils.log.RequireDebugTrue',
 		},
 	},
 	'formatters': {
-	'django.server': {
-		'()': 'django.utils.log.ServerFormatter',
-		'format': '[%(server_time)s] %(message)s',
+		'django.server': {
+			'()': 'django.utils.log.ServerFormatter',
+			'format': '[%(server_time)s] %(message)s',
 		}
 	},
 	'handlers': {
-	'console': {
-		'level': 'INFO',
-		'filters': ['require_debug_true'],
-		'class': 'logging.StreamHandler',
-	},
-	'console_debug_false': {
-		'level': 'ERROR',
-		'filters': ['require_debug_false'],
-		'class': 'logging.StreamHandler',
-	},
-	'django.server': {
-		'level': 'INFO',
-		'class': 'logging.StreamHandler',
-		'formatter': 'django.server',
-	},
-    'errors_file':{
-		'level':'ERROR',
-		'class':'logging.FileHandler',
-        'filters': ['require_debug_false'],
-		'filename':'<LOG DIRECTORY>/ErrorLoggers.log',
+		'console': {
+			'level': 'INFO',
+			'filters': ['require_debug_true'],
+			'class': 'logging.StreamHandler',
+		},
+		'console_debug_false': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'logging.StreamHandler',
+		},
+		'django.server': {
+			'level': 'INFO',
+			'class': 'logging.StreamHandler',
+			'formatter': 'django.server',
+		},
+		'errors_file':{
+			'level':'ERROR',
+			'class':'logging.FileHandler',
+			'filters': ['require_debug_false'],
+			'filename':'<LOG DIRECTORY>/ErrorLoggers.log',
 		},
 	},
 	'loggers': {
 		'django': {
-		'handlers': ['console', 'console_debug_false','errors_file'],
-		'level': 'INFO',
-	},
-	'django.server': {
-		'handlers': ['django.server'],
-		'level': 'INFO',
-		'propagate': False,
+			'handlers': ['console', 'console_debug_false','errors_file'],
+			'level': 'INFO',
+		},
+		'django.server': {
+			'handlers': ['django.server'],
+			'level': 'INFO',
+			'propagate': False,
 		}
 	}
 }
